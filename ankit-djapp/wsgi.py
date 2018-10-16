a33735d4855804867e9fe17461ffd52f86e42b68
ankit <ankitkumar751995@gmail.com>
2018-10-16 16:01:25 +0530

    Ankit

--- ankit-djapp/wsgi.py
+++ ankit-djapp/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-#from dj_static import Cling
+from dj_static import Cling
 #from whitenoise.django import DjangoWhiteNoise
 from django.conf import settings
 from django.contrib.staticfiles.handlers import StaticFilesHandler
@@ -20,11 +20,10 @@ from django.contrib.staticfiles.handlers import StaticFilesHandler
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ankit-djapp.settings')
 
 #application = get_wsgi_application()
-#application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
 #application = DjangoWhiteNoise(application)
 
-if settings.DEBUG:
-    application = StaticFilesHandler(get_wsgi_application())
-else:
-    application = get_wsgi_application()
-    
+# if settings.DEBUG:
+#     application = StaticFilesHandler(get_wsgi_application())
+# else:
+#     application = get_wsgi_application()
